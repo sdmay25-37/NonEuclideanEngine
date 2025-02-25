ba1e0da60bd851971263aec047f5702bcf67fee7
tagrinnell <tasman.grinnell@gmail.com>
2025-02-25 12:46:27 -0600

    Did some extra work with creating the TileMap Class

--- ne_engine/ne_gl/public/include/TileMap.hpp
+++ ne_engine/ne_gl/public/include/TileMap.hpp
@@ -7,20 +7,26 @@
 #define TILEMAP_HPP
 
 #include <vector>
+#include <string>
 
 struct Tile {
-    long long int tileInformation;
+    uint64_t tileInformation;
+    uint8_t tileTexture;
 };
 
 class TileMap {
 public:
     TileMap();
+    TileMap(std::vector<Tile> tileList, std::vector<Tile> seedList);
 
-    void loadTiles(std::string );
+    void loadTiles(std::string filepath); // Need to change this.  Trying to figure out a bit more about what the goal 
+    //of the function is or if we can just pass a vector in for creating the list.
+
+    std::vector <std::pair <std::string, std::string>> getTileInformation();    // Returns a vector of pairs of a <direction string, texture classification>
 
 private: 
-    std::vector <Tile> _tileList;
-    std::vector <Tile> _seedList;
+    std::vector<Tile> _tileList;
+    std::vector<Tile> _seedList;  
 };
 
 #endif // TILEMAP_HPP
--- ne_engine/ne_gl/public/src/TileMap.cpp
+++ ne_engine/ne_gl/public/src/TileMap.cpp
@@ -5,3 +5,26 @@
 
 #include "TileMap.hpp"
 
+TileMap::TileMap() {
+
+
+}
+
+TileMap::TileMap(std::vector<Tile> tileList, std::vector<Tile> seedList) {
+
+}
+
+void TileMap::loadTiles(std::string filepath) {
+
+}
+
+std::vector <std::pair <std::string, std::string>> TileMap::getTileInformation() {
+    std::vector <std::pair <std::string, std::string>> returnTiles;
+    for (int i = 0; i < _tileList.size(); i++) {
+        Tile currTile = _tileList.at(i);
+        
+        
+    }
+
+    return returnTiles;
+}    // Returns a vector of pairs of a <direction string, texture classification>
