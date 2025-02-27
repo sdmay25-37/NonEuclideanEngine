03fd52e6949a91475d6ac4cb48e1bcbaf7ca00ec
tagrinnell <tasman.grinnell@gmail.com>
2025-02-27 12:12:57 -0600

    Added Quadtree library to use as the backing structure.  I'm thinking about potentially using two strings for storage: 1 string for tile information with each character is the indentificaiton of each tile of ID x, and then using the other for a series of coordinates or tile ID

--- ne_engine/ne_gl/public/include/TileMap.hpp
+++ ne_engine/ne_gl/public/include/TileMap.hpp
@@ -3,15 +3,26 @@
 //  2/25/2025
 //
 
+/*
+    Change to Tree structure
+*/
+
 #ifndef TILEMAP_HPP
 #define TILEMAP_HPP
 
+#include "Quadtree.h"
+
 #include <vector>
 #include <string>
+#include <cstdint>
 
 struct Tile {
-    uint64_t tileInformation;
-    uint8_t tileTexture;
+    uint8_t tileId;
+    Tile* up;
+    Tile* down;
+    Tile* left;
+    Tile* right;
+    char* tileType;     // Maybe change into an enum?
 };
 
 class TileMap {
@@ -19,14 +30,15 @@ public:
     TileMap();
     TileMap(std::vector<Tile> tileList, std::vector<Tile> seedList);
 
-    void loadTiles(std::string filepath); // Need to change this.  Trying to figure out a bit more about what the goal 
-    //of the function is or if we can just pass a vector in for creating the list.
+    void loadTiles(std::string filepath);  // Need to change this.  Trying to figure out a bit more about what the goal 
+    // of the function is or if we can just pass a vector in for creating the list.
 
-    std::vector <std::pair <std::string, std::string>> getTileInformation();    // Returns a vector of pairs of a <direction string, texture classification>
+    std::vector <std::pair <char*, std::string>> getTileInformation();    // Returns a vector of pairs of a <direction string, texture classification>
 
 private: 
     std::vector<Tile> _tileList;
-    std::vector<Tile> _seedList;  
+    std::vector<Tile> _seedList;    // Standard : [origin (0, 0), up , left , right , down] 
+    // ; If there are multiple entrances, then the ordering is to go clockwise from the leftmost upper Entrances
 };
 
 #endif // TILEMAP_HPP
--- ne_engine/ne_gl/public/src/TileMap.cpp
+++ ne_engine/ne_gl/public/src/TileMap.cpp
@@ -11,19 +11,26 @@ TileMap::TileMap() {
 }
 
 TileMap::TileMap(std::vector<Tile> tileList, std::vector<Tile> seedList) {
-
+    _tileList = tileList;
+    _seedList = seedList;
 }
 
+// TODO: Recreate
 void TileMap::loadTiles(std::string filepath) {
 
 }
 
-std::vector <std::pair <std::string, std::string>> TileMap::getTileInformation() {
-    std::vector <std::pair <std::string, std::string>> returnTiles;
+std::vector <std::pair <char*, std::string>> TileMap::getTileInformation() {
+    std::vector <std::pair <char*, std::string>> returnTiles;
     for (int i = 0; i < _tileList.size(); i++) {
         Tile currTile = _tileList.at(i);
         
-        
+        int end = currTile.tileLocation & 0xF800000000000000;   // Mask the top 5 bits for the length
+        char* dirString[end];   // Create a string of size end, which is max 30 characters 
+        for (int j = 0; j < end; j++) {
+            
+        }
+
     }
 
     return returnTiles;
