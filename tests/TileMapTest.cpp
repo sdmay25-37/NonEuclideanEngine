23d69e6f9a92baa20664e79a3bb1ee5d49ca6be4
CoryRoth <cgroth@iastate.edu>
2025-03-29 15:10:38 -0500

    cleaned up includes

--- tests/TileMapTest.cpp
+++ tests/TileMapTest.cpp
@@ -1,20 +1,6 @@
-#define GLM_FORCE_CTOR_INIT
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
 #include <iostream>
 #include <cstdlib>
-#include <ctime>
-#include <chrono>
-#include <thread>
-
 #include "ne_engine.hpp"
-
 #include "TileMap.hpp"
 #include "Tile.hpp"
 
