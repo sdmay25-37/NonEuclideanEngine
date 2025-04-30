0dbdeb48d77b5a16b56619100624c8e32b2c868b
benjisu <benj2003@iastate.edu>
2025-04-30 16:59:03 -0500

    small fixes

--- ne_engine/ne_graphics/public/src/Renderer.cpp
+++ ne_engine/ne_graphics/public/src/Renderer.cpp
@@ -16,6 +16,8 @@ void Renderer::Init() {
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 	glGenBuffers(1, &UV_VBO);
@@ -86,16 +88,14 @@ void Renderer::Init() {
 	}
 
 	_shader_program = std::make_unique<ShaderProgram>(shader_result.ok());
-	_shader_program->bind();
-	_shader_program->setUniform1i("texture_atlas", 0);
 }
 
 // Todo: Not sure how I feel about this method
 // I don't like having to copy UV data every frame when it likely doesn't change
 void Renderer::Render(entt::registry& registry, Resource<Camera> camera) const {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	_shader_program->bind();
 	_shader_program->setUniformMat4("proj_view_mat", camera->GetViewProjMat());
 
 	auto const view = registry.view<AtlasSprite>();
@@ -133,7 +133,7 @@ void Renderer::Render(entt::registry& registry, Resource<Camera> camera) const {
 
 		// Buffer UV data
 		glBindBuffer(GL_ARRAY_BUFFER, UV_VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec4) * uv_ranges.size(), uv_ranges.data(), GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec4) * uv_ranges.size(), uv_ranges.data(), GL_DYNAMIC_DRAW);
 
 		// Bind atlas texture
 		glActiveTexture(GL_TEXTURE0);
